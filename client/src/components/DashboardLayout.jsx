65108214663908081e62a3ac666b0fa0beb94e39
stablequote <asaad__2015@hotmail.com>
2025-01-27 17:27:22 +0200

    added hamburger menu icon

--- client/src/components/DashboardLayout.jsx
+++ client/src/components/DashboardLayout.jsx
@@ -1,15 +1,21 @@
-import { AppShell, Header, Flex, Button, Group, Stack, Footer, Box, Text } from '@mantine/core';
+import { AppShell, Header, Flex, Button, Group, Stack, Footer, Box, Text, Drawer, Burger } from '@mantine/core';
 import AppNavbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import { useDisclosure } from '@mantine/hooks';
 
 function DashboardWrapper() {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <AppShell
       padding="md"
-      navbar={<AppNavbar />}
+      // navbar={<AppNavbar />}
       header={
         <Header height={60} p="xs" sx={{backgroundColor: '#1D242E'}}>
+          <Flex>
+            <Burger color='white' mr="lg" onClick={open} />
           <Text color='white' fw="bold" fz="lg">Pharmacia</Text>
+          </Flex>
         </Header>
       }
       // footer={
@@ -23,6 +29,7 @@ function DashboardWrapper() {
     >
       {/* Your application here */}
       <Outlet />
+        <AppNavbar />
     </AppShell>
   );
 }
