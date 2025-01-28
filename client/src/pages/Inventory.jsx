a94755bd082cd5c60032d27805f7065e7ae78f83
stablequote <asaad__2015@hotmail.com>
2025-01-29 00:17:47 +0200

    added import button on Inventory page

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
-import {Box, Button, Container, Flex, StarIcon, Text, TextInput} from "@mantine/core"
+import React, { useRef } from 'react';
+import {Box, Button, Container, FileInput, Flex, StarIcon, Text, TextInput} from "@mantine/core"
 import DataGrid from '../components/DataGrid';
-import { IconFileSearch, IconInputSearch } from '@tabler/icons-react';
+import { IconFileImport, IconFileSearch, IconInputSearch } from '@tabler/icons-react';
 
 const Inventory = () => {
+  const openRef = useRef(null);
+
   return (
     <>
       <Container size="lg">
         {/* <Text fz="lg" fw="bold" align="center">Inventory Page</Text> */}
         <Box>
           <Text>Search Medication/Product</Text>
-          <Flex justify="flex-start" sx={{width: 350}} height={415}>
-            <TextInput placeholder='enter product name/ID' sx={{width: 320}} height={415} mb={4} />
-            <Button variant="filled" color='blue' sx={{borderRadius: 50}} ml={15}>
-              <IconInputSearch color='white' size={25} />
-            </Button>
+          <Flex justify="space-between"  height={415}>
+            <Flex >
+              <TextInput placeholder='enter product name/ID' sx={{width: 320}} height={415} mb={4} />
+              <Button variant="filled" color='blue' sx={{borderRadius: 50}} ml={15}>
+                <IconInputSearch color='white' size={25} />
+              </Button>
+            </Flex>
+            {/* <Button variant="filled" color="green" onClick={() => openRef.current()}>Import from Excel</Button> */}
+            <FileInput
+              styles={(theme) => ({
+                input: {
+                  backgroundColor: "green", // Green background
+                  "&:hover": {
+                    backgroundColor: "#026602", // Slightly darker green on hover
+                  },
+                },
+                placeholder: {
+
+                  color: "white !important"
+                },
+                label: {
+                  color: "white", // White label color
+                },
+                icon: {
+                  color: "white"
+                }
+              })}
+              placeholder="Import from Excel" 
+              accept="xls, xlsx, csv" 
+              color='blue' 
+              variant="filled" 
+              icon={<IconFileImport />}  
+            />
           </Flex>
         </Box>
         <DataGrid />
