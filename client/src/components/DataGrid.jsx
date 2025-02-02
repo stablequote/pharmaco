8351a4428f9775e0f41df1aa1421eed80b7ee981
stablequote <asaad__2015@hotmail.com>
2025-02-03 00:13:29 +0200

    cleaned up data grid component according to DTY principle

--- client/src/components/DataGrid.jsx
+++ client/src/components/DataGrid.jsx
@@ -2,119 +2,15 @@ import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
 import { Box, Button } from '@mantine/core';
 import { IconDownload } from '@tabler/icons-react';
 import { mkConfig, generateCsv, download } from 'export-to-csv';
-import moment from 'moment'
 
-const columns = [
-  {
-    accessorKey: 'barcodeID',
-    header: 'Barcode',
-    size: 40,
-  },
-  {
-    accessorKey: 'product',
-    header: 'Product',
-    size: 120,
-  },
-  {
-    accessorKey: 'shelf',
-    header: 'Shelf',
-    size: 40,
-  },
-  {
-    accessorKey: 'quantity',
-    header: 'Quantity',
-    size: 120,
-    Cell: ({ cell }) => (
-      <Box
-        sx={(theme) => ({
-          backgroundColor:
-            cell.getValue() < 100 ? theme.colors.red[9] : theme.colors.white,
-          borderRadius: '4px',
-          color: cell.getValue() < 100 ? 'white' : theme.colors.blue,
-          maxWidth: '9ch',
-          padding: '4px',
-        })}
-      >
-        {cell.getValue()?.toLocaleString?.('en-US', {
-          currency: 'SDG',
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 0,
-        })}
-      </Box>
-    ),
-  },
-  {
-    accessorKey: 'unit',
-    header: 'Unit',
-    size: 120,
-  },
-  {
-    accessorKey: 'expiryDate',
-    header: 'Expiry Date',
-    size: 100,
-    mantineEditTextInputProps: ({ cell }) => ({
-      ...getCommonEditTextInputProps(cell),
-    }),
-    Cell: ({ cell }) => (
-      // onclick( () => console.log(cell))
-      <Box>
-        {moment(cell.getValue()).format("DD-MMMM-YYYY")}
-
-      </Box>
-    ),
-  },
-  {
-    accessorKey: 'unitPurchasePrice',
-    header: 'Purchase Price',
-    size: 100,
-    Cell: ({ cell }) => (
-      <Box
-        sx={(theme) => ({
-          borderRadius: '4px',
-          maxWidth: '9ch',
-          padding: '4px',
-        })}
-      >
-        {cell.getValue()?.toLocaleString?.('en-US', {
-          style: 'currency',
-          currency: 'SDG',
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 0,
-        })}
-      </Box>
-    ),
-  },
-  {
-    accessorKey: 'unitSalePrice',
-    header: 'Sale Price',
-    size: 100,
-    Cell: ({ cell }) => (
-      <Box
-        sx={(theme) => ({
-          borderRadius: '4px',
-          maxWidth: '9ch',
-          padding: '4px',
-        })}
-      >
-        {cell.getValue()?.toLocaleString?.('en-US', {
-          style: 'currency',
-          currency: 'SDG',
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 0,
-        })}
-      </Box>
-    ),
-  },
-];
-
-const csvConfig = mkConfig({
-  fieldSeparator: ',',
-  decimalSeparator: '.',
-  useKeysAsHeaders: true,
-});
-
-const DataGrid = ({ inventoryData = [] }) => {
+const DataGrid = ({ data, columns}) => {
   
+  const csvConfig = mkConfig({
+    fieldSeparator: ',',
+    decimalSeparator: '.',
+    useKeysAsHeaders: true,
+  });
+
   const handleExportRows = (rows) => {
     const rowData = rows.map((row) => row.original);
     const csv = generateCsv(csvConfig)(rowData);
@@ -127,8 +23,8 @@ const DataGrid = ({ inventoryData = [] }) => {
   };
 
   const table = useMantineReactTable({
-    columns,
-    data: inventoryData, // Use the correct prop name
+    columns: columns,
+    data: data, // Use the correct prop name
     enableRowSelection: true,
     columnFilterDisplayMode: 'popover',
     paginationDisplayMode: 'pages',
@@ -195,7 +91,7 @@ const DataGrid = ({ inventoryData = [] }) => {
       table={table}
       positionGlobalFilter="right"
       mantineSearchTextInputProps={{
-        placeholder: `Search ${inventoryData.length} rows`,
+        placeholder: `Search ${data.length} rows`,
         sx: { minWidth: '300px' },
         variant: 'filled',
       }}
