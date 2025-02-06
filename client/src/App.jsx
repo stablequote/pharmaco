b8624dcea592667f036cf1b20dd955bcc4f12dba
stablequote <asaad__2015@hotmail.com>
2025-02-06 23:27:49 +0200

    configred protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,22 +12,25 @@ import Orders from './pages/Orders';
 import Suppliers from './pages/Suppliers';
 import ProductSales from './pages/ProductSales';
 import Login from './pages/Login';
+import PrivateRoute from './pages/ProtectedRoute';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-          <Route index  element={<Login />} />
-        <Route path="/" element={<DashboardLayout />}>
-          <Route path="home" element={<Home />} />
-          <Route path="inventory" element={<Inventory />} />
-          <Route path="pos" element={<PosPage />} />
-          <Route path="verify" element={<VerifiyTransaction />} />
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="suppliers" element={<Suppliers />} />
-          <Route path="sales" element={<ProductSales />} />
-        </Route>
+          {/* <Route index  element={<Login />} /> */}
+          <Route element={<PrivateRoute allowedRoles={['owner', 'manager']} />}>
+            <Route path="/" element={<DashboardLayout />}>
+              <Route path="home" element={<Home />} />
+              <Route path="inventory" element={<Inventory />} />
+              <Route path="pos" element={<PosPage />} />
+              <Route path="verify" element={<VerifiyTransaction />} />
+              <Route path="analytics" element={<Analytics />} />
+              <Route path="orders" element={<Orders />} />
+              <Route path="suppliers" element={<Suppliers />} />
+              <Route path="sales" element={<ProductSales />} />
+            </Route>
+          </Route>
         {/* Default Route */}
         <Route path='/login' element={<Login />} />
       </Routes>
--- client/src/pages/ProtectedRoute.jsx
+++ client/src/pages/ProtectedRoute.jsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 const PrivateRoute = ({ allowedRoles }) => {
-  const token = localStorage.getItem("authToken");
-  const user = token ? JSON.parse(atob(token.split(".")[1])) : null; // Decode token payload
-  const navigate = useNavigate()
-  if (!token) {
-    // return <Navigate to="/login" />;
-    return navigate("/login")
-  }
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem("authToken");
+        const user = token ? JSON.parse(atob(token.split(".")[1])) : null; // Decode token payload
+
+        if(!token) {
+            return navigate("/login")
+        }
+    }, [])
+    
+//   const token = localStorage.getItem("authToken");
+//   const user = token ? JSON.parse(atob(token.split(".")[1])) : null; // Decode token payload
+//   const navigate = useNavigate()
+//   if (!token) {
+//     // return <Navigate to="/login" />;
+//     return navigate("/login")
+//   }
 
   return <Outlet />;
 };
