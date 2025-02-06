2f4b32d30b5165cecddecf8b4ae7e3a8a4395761
stablequote <asaad__2015@hotmail.com>
2025-02-07 00:23:07 +0200

    added logout route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/user.controller');
 // Authentication routes
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router;
