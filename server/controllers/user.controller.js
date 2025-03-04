a4ddb43ed9fa0fb6ee41ac1130cc9fb497325df6
stablequote <asaad__2015@hotmail.com>
2025-03-04 10:56:56 +0200

    installed pkg to bundle .exe, and changed conflicting packages

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model'); // Import User schema
 
@@ -39,7 +39,7 @@ exports.login = async (req, res) => {
 
         const token = jwt.sign(
             { id: user._id, role: user.role },
-            process.env.JWT_SECRET,
+            "lion64",
             { expiresIn: '1d' }
         );
 
@@ -54,3 +54,12 @@ exports.logout = (req, res) => {
     // For now, we'll just respond with a success message
     res.status(200).json({ message: "Logout successful." });
 };
+
+exports.findAllUSers = async (req, res) => {
+    const users = await User.find({});
+    if(!users) {
+        res.status(404).send({message: "no users found"})
+    } else {
+        return res.send(users);
+    }
+}
